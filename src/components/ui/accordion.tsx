99835385df951930c758d161c939d1f7904fd025
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-06-17 23:53:11 +0300

    Chore: accordion scale

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -19,7 +19,7 @@ function AccordionItem({
 
   return (
     <motion.div
-      whileHover={{ scale: 1.2, transition: { duration: 0.3 } }}
+      whileHover={{ scale: 1.02, transition: { duration: 0.3 } }}
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3, ease: "easeOut" }}
