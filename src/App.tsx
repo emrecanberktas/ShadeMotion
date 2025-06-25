fe70fca6d59928fcc7abbe99d7c5c4ebd30581db
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-06-25 17:51:17 +0300

    Finish: Dialog

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,16 @@ import {
   DropdownMenuSubContent,
   DropdownMenuPortal,
 } from "./components/ui/dropdown-menu";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+  DialogTitle,
+} from "./components/ui/dialog";
 
 function App() {
   const [buttonState, setButtonState] = useState<
@@ -276,6 +286,53 @@ function App() {
                   <AlertDescription>Alert Description</AlertDescription>
                 </Alert>
               )}
+            </div>
+          </div>
+          {/*Dialog Section*/}
+
+          <div className="mt-8 bg-white dark:bg-zinc-900 p-6 rounded-lg shadow-sm">
+            <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-white">
+              Dialog Example
+            </h2>
+            <div className="flex justify-center items-center">
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="outline">Open Dialog</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your profile here. Click save when you're
+                      done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4">
+                    <div className="grid gap-3">
+                      <label htmlFor="name-1">Name</label>
+                      <input
+                        id="name-1"
+                        name="name"
+                        defaultValue="Pedro Duarte"
+                      />
+                    </div>
+                    <div className="grid gap-3">
+                      <label htmlFor="username-1">Username</label>
+                      <input
+                        id="username-1"
+                        name="username"
+                        defaultValue="@peduarte"
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    <Button type="submit">Save changes</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             </div>
           </div>
 
