9a3e35e04a05cc70f8022b03538802ed834f9ff8
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-06-26 14:32:30 +0300

    Fix: tab initial coordinate

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -95,9 +95,8 @@ function TabsContent({
       {...props}
     >
       <motion.div
-        initial={{ opacity: 0, y: -25 }}
+        initial={{ opacity: 0, y: -10 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 25 }}
         transition={{ duration: 0.5 }}
       >
         {props.children}
